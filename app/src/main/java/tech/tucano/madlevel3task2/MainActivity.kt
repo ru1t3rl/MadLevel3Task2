5e42232f2546ae5005a28dae99e0119276d86b52
ru1t3rl <luca.ruiters@hva.nl>
2020-09-11 09:04:55 +0100

    Setup the home screen

--- app/src/main/java/tech/tucano/madlevel3task2/MainActivity.kt
+++ app/src/main/java/tech/tucano/madlevel3task2/MainActivity.kt
@@ -6,18 +6,16 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import tech.tucano.madlevel3task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/tech/tucano/madlevel3task2/PortalsFragment.kt
+++ app/src/main/java/tech/tucano/madlevel3task2/PortalsFragment.kt
@@ -7,25 +7,29 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import tech.tucano.madlevel3task2.databinding.FragmentPortalsBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class PortalsFragment : Fragment() {
+    private lateinit var binding: FragmentPortalsBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_portals, container, false)
+        binding = FragmentPortalsBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.fab.setOnClickListener{
+            findNavController().navigate(R.id.action_portalsFragment_to_addPortalFragment)
         }
     }
 }
